78e4cd21b05b92bbc83b11e782ba14d2f8eae8e9
pashk <pavvvvvel>
2022-06-07 22:25:50 +0300

    Create empty CountryDetailsFragment.kt

---
+++ app/src/main/java/com/example/hstesttask/app/country_details/CountryDetailsFragment.kt
@@ -0,0 +1,8 @@
+package com.example.hstesttask.app.country_details
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CountryDetailsFragment : Fragment() {
+}
