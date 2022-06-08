2ae9f70e136bcb991776b95af7f1d309e1c07495
pashk <pavvvvvel>
2022-06-08 15:52:00 +0300

    Added binding to CountryDetailsFragment.kt

--- app/src/main/java/com/example/hstesttask/app/country_details/CountryDetailsFragment.kt
+++ app/src/main/java/com/example/hstesttask/app/country_details/CountryDetailsFragment.kt
@@ -1,8 +1,28 @@
 package com.example.hstesttask.app.country_details
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.hstesttask.databinding.FragmentCountryDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CountryDetailsFragment : Fragment() {
+
+    private lateinit var binding: FragmentCountryDetailsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentCountryDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
